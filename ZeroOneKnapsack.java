3b90ce0530b8fb9a532815f0fb6cede6902fc099
tanmay451 <tanmaypiyushkaushik451@gmail.com>
2021-09-08 22:03:51 +0530

    fix for memoization

--- ZeroOneKnapsack.java
+++ ZeroOneKnapsack.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ZeroOneKnapsack {
 	static int max(int a, int b) {
 		return (a > b) ? a : b;
@@ -18,20 +19,21 @@ public class ZeroOneKnapsack {
 	static int knapsackMemoization(int[] wt, int[] val, int W, int N) {
 		if (W == 0 || N == 0)
 			return 0;
-
+		
 		if (t[W][N] != -1)
 			return t[W][N];
+		
 		if (wt[N - 1] <= W) {
-			return max(val[N - 1] + knapsackMemoization(wt, val, W - wt[N - 1], N - 1),
+			return t[W][N] = max(val[N - 1] + knapsackMemoization(wt, val, W - wt[N - 1], N - 1),
 					knapsackMemoization(wt, val, W, N - 1));
 		} else
-			return knapsackMemoization(wt, val, W, N - 1);
+			return t[W][N] = knapsackMemoization(wt, val, W, N - 1);
 	}
 
 	public static void main(String[] args) {
-		int[] wt = { 1, 3, 4 };
-		int[] val = { 1, 8, 3 };
-		int W = 5;
+		int[] wt = { 1, 3, 4,1,1,1,1, };
+		int[] val = { 1, 8, 3,1,1,1,1 };
+		int W = 7;
 
 		for (int i = 0; i < 100; i++) {
 			for (int j = 0; j < 100; j++) {
@@ -39,7 +41,7 @@ public class ZeroOneKnapsack {
 			}
 		}
 
-		System.out.println("0/1 Knapsack using recurtion:\t" + knapsackRecursive(wt, val, W, 3));
-		System.out.println("0/1 Knapsack using memoization:\t" + knapsackMemoization(wt, val, W, 3));
+		System.out.println("0/1 Knapsack using recurtion:\t" + knapsackRecursive(wt, val, W, 7));
+		System.out.println("0/1 Knapsack using memoization:\t" + knapsackMemoization(wt, val, W, 7));
 	}
 }
