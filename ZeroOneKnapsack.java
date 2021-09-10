d74453d988362054ed5a6cc516d3cfa93416e4a2
tanmay451 <tanmaypiyushkaushik451@gmail.com>
2021-09-10 11:23:50 +0530

    knapsack using dp/top-down

--- ZeroOneKnapsack.java
+++ ZeroOneKnapsack.java
@@ -3,7 +3,8 @@ public class ZeroOneKnapsack {
 		return (a > b) ? a : b;
 	}
 
-	static int[][] t = new int[100][100];
+	static int[][] m = new int[100][100];
+	static int[][] dp = new int[100][100];
 
 	static int knapsackRecursive(int[] wt, int[] val, int W, int n) {
 		if (W == 0 || n == 0)
@@ -19,14 +20,34 @@ public class ZeroOneKnapsack {
 		if (W == 0 || N == 0)
 			return 0;
 
-		if (t[W][N] != -1)
-			return t[W][N];
+		if (m[W][N] != -1)
+			return m[W][N];
 
 		if (wt[N - 1] <= W) {
-			return t[W][N] = max(val[N - 1] + knapsackMemoization(wt, val, W - wt[N - 1], N - 1),
+			return m[W][N] = max(val[N - 1] + knapsackMemoization(wt, val, W - wt[N - 1], N - 1),
 					knapsackMemoization(wt, val, W, N - 1));
 		} else
-			return t[W][N] = knapsackMemoization(wt, val, W, N - 1);
+			return m[W][N] = knapsackMemoization(wt, val, W, N - 1);
+	}
+
+	static int knapsackDP(int[] wt, int[] val, int W, int N) {
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= W; j++) {
+				if (i == 0 || j == 0) {
+					dp[i][j] = 0;
+				}
+			}
+		}
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= W; j++) {
+				if (wt[i - 1] > j)
+					dp[i][j] = dp[i - 1][j];
+				else
+					dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+			}
+		}
+		return dp[N][W];
 	}
 
 	public static void main(String[] args) {
@@ -36,11 +57,12 @@ public class ZeroOneKnapsack {
 
 		for (int i = 0; i < 100; i++) {
 			for (int j = 0; j < 100; j++) {
-				t[i][j] = -1;
+				m[i][j] = -1;
 			}
 		}
 
 		System.out.println("0/1 Knapsack using recurtion:\t" + knapsackRecursive(wt, val, W, 7));
 		System.out.println("0/1 Knapsack using memoization:\t" + knapsackMemoization(wt, val, W, 7));
+		System.out.println("0/1 Knapsack using DP:\t" + knapsackDP(wt, val, W, 7));
 	}
 }
