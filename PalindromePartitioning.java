bc4a4d6071bfa7109a18df2469f09dbf9893a890
tanmay451 <tanmaypiyushkaushik451@gmail.com>
2021-09-26 10:06:21 +0530

    added more optimized version for memoization

--- PalindromePartitioning.java
+++ PalindromePartitioning.java
@@ -45,15 +45,50 @@ public class PalindromePartitioning {
         return m[i][j] = min;
     }
 
+    static int palindromePartitioningMemoizationOptimized(String str, int i, int j) {
+        if (i >= j || isPalindrome(str, i, j))
+            return 0;
+        if (m[i][j] != -1) {
+            return m[i][j];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int t1;
+            if (m[i][k] != -1) {
+                t1 = m[i][k];
+            } else {
+                t1 = palindromePartitioningMemoizationOptimized(str, i, k);
+            }
+
+            int t2;
+            if (m[k + 1][j] != -1) {
+                t2 = m[k + 1][j];
+            } else {
+                t2 = palindromePartitioningMemoizationOptimized(str, k + 1, j);
+            }
+
+            int temp = t1 + t2 + 1;
+            if (temp < min) {
+                min = temp;
+            }
+        }
+        return m[i][j] = min;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Palindrome Partitioning:\t" + palindromePartitioning("geekeega", 0, 7));
+
+        String str = "geegee";
+
+        System.out.println("Palindrome Partitioning:\t" + palindromePartitioning(str, 0, str.length() - 1));
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
                 m[i][j] = -1;
             }
         }
-        System.out.println(
-                "Palindrome Partitioning using Memoization:\t" + palindromePartitioningMemoization("geekeega", 0, 7));
+        System.out.println("Palindrome Partitioning using Memoization:\t"
+                + palindromePartitioningMemoization(str, 0, str.length() - 1));
+        System.out.println("Palindrome Partitioning using Memoization optimized:\t"
+                + palindromePartitioningMemoizationOptimized(str, 0, str.length() - 1));
 
     }
 }
